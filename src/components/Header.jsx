641b72ac8c9cee92af49d62a0d19bcd3cb8a2f50
Lucas Pereyra <luuucasp31@gmail.com>
2025-07-16 12:40:29 -0300

    Componente Card

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import logoTolo from "../assets/logoTolo.png"
 export default function HeaderNav() {
   return (
     <header className='bg-black h-20 sm:h-12 md:h-20 lg:h-32 flex items-center'>
-      <img src={logoTolo} alt="Logo Tolo" className='w-10 h-10 sm:w-12 sm:h-12 md:w-32 md:h-32 lg:w-40 lg:h-40' />
+      <img src={logoTolo} alt="Logo Tolo" className='w-10 h-10 sm:w-12 sm:h-12 md:w-32 md:h-36 lg:w-40 lg:h-40' />
     </header>
   )
 }
