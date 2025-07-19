108949d938fe63e550263e7973cd1bef08da1b2d
Lucas Pereyra <luuucasp31@gmail.com>
2025-07-19 13:14:01 -0300

    button update

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { useForm } from 'react-hook-form'
 import logoToloBlue from "../assets/logoToloBlue.png"
 
-export default function Form({ fields, onSubmit, title, description, logo, className }) {
+export default function Form({ fields, onSubmit, button, title, description, logo, className }) {
     const { register, handleSubmit, watch, formState: { errors } } = useForm()
     return (
         <form action="" onSubmit={handleSubmit((data) => onSubmit(data, watch))} className={`w-75 bg-white m-auto p-3 shadow rounded-xl ${className}`}>
@@ -10,7 +10,7 @@ export default function Form({ fields, onSubmit, title, description, logo, class
             <h2 className='font-[Montserrat,sans-serif] text-2xl font-semibold'>{title}</h2>
             <p className="text-sm text-gray-600">{description}</p>
             {fields.map((input, index) => React.cloneElement(input, { register, errors, watch, key: index }))}
-            <button type='submit'>enviar</button>
+            {button}
         </form>
     )
 }
