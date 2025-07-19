e2d663182b2f4973713f44f8d2627157cbbb5ef3
Lucas Pereyra <luuucasp31@gmail.com>
2025-07-18 23:29:56 -0300

    Input Update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Form from '../components/Form'
+import Input from '../components/Input'
 
 export default function Home() {
   return (
     <>
-      
+    
     </>
   )
 }
