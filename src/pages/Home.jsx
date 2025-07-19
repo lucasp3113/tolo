0b3cfba9ebc0a0307b335f34ed84dfe4f4be4545
Lucas Pereyra <luuucasp31@gmail.com>
2025-07-18 23:34:04 -0300

    Uní input a rama principal

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
-import Form from '../components/Form'
-import Input from '../components/Input'
 
 export default function Home() {
   return (
     <>
-    
+      
     </>
   )
 }
