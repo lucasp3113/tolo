c75fd27b112ccc1d0c79590edebc6b454291ef47
Lucas Pereyra <luuucasp31@gmail.com>
2025-07-20 21:32:05 -0300

    update HeaderNav

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -33,30 +33,34 @@
 
 import React from 'react';
 
-export default function Menu({elements, model3d}) {
+export default function Menu({ elements, model3d }) {
     return (
-            <ul className="m-3 flex items-center justify-between">
-                {elements.map((element, index) => (
-                    <li onClick={element.onClick}
-                        className={`group text-white cursor-pointer flex items-center m-3 
+        <ul className="m-3 flex items-center justify-between">
+            {elements.map((element, index) => (
+                <li onClick={element.onClick}
+                    className={`group text-white cursor-pointer flex items-center m-3 
                         ${element.animation
-                                ? "relative leading-none transition-transform duration-300 hover:scale-110 after:content-[''] after:absolute after:left-1/2 after:-translate-x-1/2 after:-bottom-[3px] after:h-[2px] after:w-0 after:bg-white after:transition-all after:duration-700 hover:after:w-full"
-                                : ""
-                            }`}
-                        key={index}
+                            ? "relative leading-none transition-transform duration-300 hover:scale-110 after:content-[''] after:absolute after:left-1/2 after:-translate-x-1/2 after:-bottom-[3px] after:h-[2px] after:w-0 after:bg-white after:transition-all after:duration-700 hover:after:w-full"
+                            : ""
+                        }`}
+                    key={index}
+                >
+                    <span
+                        className={`${element.icon.expand
+                            ? "opacity-0 max-w-0 overflow-hidden group-hover:opacity-100 group-hover:sm:max-w-[100px] group-hover:md:max-w-[150px] group-hover:lg:max-w-[200px] transition-all duration-900 ease-in-out whitespace-nowrap inline-block font-[Montserrat,sans-serif] m-2 sm:text-[8px] md:text-[20px] lg:text-[20px]"
+                            : ""
+                            } group-hover:inline-block font-[Montserrat,sans-serif] m-2 sm:text-[8px] md:text-[20px] lg:text-[20px]`}
                     >
-                        <span
-                            className={`${element.icon.expand
-                                ? "opacity-0 max-w-0 overflow-hidden group-hover:opacity-100 group-hover:sm:max-w-[100px] group-hover:md:max-w-[150px] group-hover:lg:max-w-[200px] transition-all duration-900 ease-in-out whitespace-nowrap inline-block font-[Montserrat,sans-serif] m-2 sm:text-[8px] md:text-[20px] lg:text-[20px]"
-                                : ""
-                                } group-hover:inline-block font-[Montserrat,sans-serif] m-2 sm:text-[8px] md:text-[20px] lg:text-[20px]`}
-                        >
-                            {element.title}
-                        </span>
-                        {element.icon.name}
-                    </li>
-                ))}
-            {model3d ? model3d : undefined}
-            </ul>
+                        {element.title}
+                    </span>
+                    {element.icon.name}
+                </li>
+            ))}
+            {model3d?.map((model, index) => (
+                <li key={`model3d-${index}`}>
+                    {model}
+                </li>
+            ))}
+        </ul>
     );
 }
