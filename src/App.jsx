0f00a6fd79d4b9193212cc4b5c3becbb0c61f1cf
Lucas Pereyra <luuucasp31@gmail.com>
2025-07-22 13:13:08 -0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
 
-
 function App() {
   return(<div className="Tolo">
     <BrowserRouter>
