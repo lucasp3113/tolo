9c75c77a8fc80729959be3eb5716fa8fe0b619cc
Lucas Pereyra <luuucasp31@gmail.com>
2025-07-26 15:01:00 -0300

    update vibration

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,8 +2,6 @@ import React from 'react'
 import Form from '../components/Form';
 import Input from '../components/Input'
 import { IoSearch } from "react-icons/io5";
-import Tes from '../components/Tes';
-
 
 
 export default function Home() {
@@ -11,7 +9,7 @@ export default function Home() {
     <div className='flex items-center justify-center'>
       <Form className={"!bg-transparent !shadow-none !border-none !rounded-none"} fields={[
         <Input type={"text"}
-         name={"search"} className='pr-10' icon={<IoSearch className='text-2xl text-gray-600'/>} placeholder={"Buscar"}/>
+         name={"search"} className='pr-10' icon={<IoSearch className='-translate-y-1/2 text-2xl text-gray-600'/>} placeholder={"Buscar"}/>
       ]}/>
     </div>
   )
