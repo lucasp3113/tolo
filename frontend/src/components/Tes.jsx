ab30ac32dec73a05b8e31a7057e69641b119da8a
Lucas Pereyra <luuucasp31@gmail.com>
2025-07-26 00:14:11 -0300

    update

---
+++ frontend/src/components/Tes.jsx
@@ -0,0 +1,9 @@
+import '../index.css';
+
+export default function Tes() {
+  return (
+    <div className="animation-shake a border border-black p-5">
+      Probando animación shake
+    </div>
+  )
+}
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Form from '../components/Form';
 import Input from '../components/Input'
 import { IoSearch } from "react-icons/io5";
+import Tes from '../components/Tes';
+
 
 
 export default function Home() {
@@ -9,8 +11,9 @@ export default function Home() {
     <div className='flex items-center justify-center'>
       <Form className={"!bg-transparent !shadow-none !border-none !rounded-none"} fields={[
         <Input type={"text"}
-         name={"search"} icon={<IoSearch className='text-2xl text-gray-600'/>} placeholder={"Buscar"}/>
+         name={"search"} className='pr-10' icon={<IoSearch className='text-2xl text-gray-600'/>} placeholder={"Buscar"}/>
       ]}/>
+      <Tes/>
     </div>
   )
 }
