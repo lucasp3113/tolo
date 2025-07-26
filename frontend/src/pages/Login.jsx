600515b24f277d56fef261e474df86d91983b4f6
Lucas Pereyra <luuucasp31@gmail.com>
2025-07-26 15:01:00 -0300

    update vibration

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -2,12 +2,17 @@ import React from 'react'
 import Form from '../components/Form'
 import Input from '../components/Input'
 import Button from '../components/Button'
+import { useState } from 'react'
+import { FaUserCircle } from 'react-icons/fa'
+import { HiLockClosed, HiEye, HiEyeOff } from 'react-icons/hi'
 
 export default function Login() {
+    const [showPassword, setShowPassword] = useState(false)
     return (
         <div className="flex items-center justify-center m-5">
             <Form google={true} remember={true} logo={true} button={<Button className={"w-50"} color={"blue"} size={"md"} text={"Iniciar sesión"} />} className={" "} title={"Login"} description={"Completá el formulario para iniciar sesion."} fields={[
                 <Input
+                    icon={<FaUserCircle/>}
                     type={"text"}
                     name={"user"}
                     placeholder={"Usuario"}
@@ -15,8 +20,13 @@ export default function Login() {
                     label={"Usuario"}
                 />,
                 <Input
+                    icon={
+                        <div onClick={() => setShowPassword(!showPassword)} className="cursor-pointer">
+                            {showPassword ? <HiEye /> : <HiEyeOff />}
+                        </div>
+                    }
                     label={"Contraseña"}
-                    type={"password"}
+                    type={showPassword ? "text" : "password"}
                     name={"password"}
                     placeholder={"Contraseña"}
                     required={true}
