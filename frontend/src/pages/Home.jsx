ef02b80261dd235d409499f86140a4b9c648c0f3
Lucas Pereyra <luuucasp31@gmail.com>
2025-08-03 21:19:47 -0300

    ProductCard

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,26 +3,32 @@ import Form from '../components/Form';
 import Input from '../components/Input'
 import { IoSearch } from "react-icons/io5";
 import { useState, useEffect } from 'react';
+import ProductCard from '../components/ProductCard';
+import pelota from '../assets/pelota.png'
 
 export default function Home() {
   const [windowWidth, setWindowWidth] = useState(window.innerWidth)
   useEffect(() => {
-      const handleResize = () => setWindowWidth(window.innerWidth);
-      window.addEventListener("resize", handleResize);
-      
-      return () => {
-        window.removeEventListener("resize", handleResize);
-      };
-    }, []);
+    const handleResize = () => setWindowWidth(window.innerWidth);
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
   return (
-    <div className='flex items-center justify-center'>
+    <div className='flex items-center flex-col justify-center'>
       {windowWidth < 500 ? (
         <Form className={"!bg-transparent !shadow-none !border-none !rounded-none"} fields={[
-        <Input type={"text"}
-         name={"search"} className='pr-10' icon={<IoSearch className='-translate-y-1/2 text-2xl text-gray-600'/>} placeholder={"Buscar"}/>
-      ]}/>
+          <Input type={"text"}
+            name={"search"} className='pr-10' icon={<IoSearch className='-translate-y-1/2 text-2xl text-gray-600' />} placeholder={"Buscar"} />
+        ]} />
       ) : undefined}
-      
+      <div className="flex w-full items-center justify-center">
+        <ProductCard name={"Pelota nike"} price={3000} image={pelota} stock={10} freeShipping={true} />
+        
+      </div>
+
     </div>
   )
 }
--- frontend/src/pages/SellerDashboard.jsx
+++ frontend/src/pages/SellerDashboard.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 export default function SellerDashboard() {
   return (
     <div>
-        <h1 className='text-3xl'>Perfil</h1>
+        <h1 className="text-3xl mt-5 font-semibold text-black tracking-wide">Panel de control</h1>
+
     </div>
   )
 }
